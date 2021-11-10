ba97f8d6777352fcbeceba407e40b9728a6e8d74
anderav1 <avabc5@mail.umsl.edu>
2021-11-10 15:37:41 -0600

    Implement resource descriptors as described in lecture

--- shm.h
+++ shm.h
@@ -3,4 +3,26 @@
 // CS 4760, Project 5
 // shm.h -- Implementation of shared memory
 
+#ifndef SHM_H
+#define SHM_H
 
+#include <stdlib.h>
+
+#define MAX_USER_PROCS 18
+
+// termination criteria
+#define MAX_PROCS_GENERATED 40
+#define MAX_TIME 5
+
+
+// TODO: shared memory, semaphores, message queues
+
+// allocate shared memory for system data structs
+	// resource descriptors
+
+typedef struct {
+
+} Resource;
+
+
+#endif
