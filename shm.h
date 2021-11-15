0283e1f21e4909e775ad415ef5cc5119072a2def
anderav1 <avabc5@mail.umsl.edu>
2021-11-15 15:51:02 -0600

    Implementing resource descriptors

--- shm.h
+++ shm.h
@@ -1,5 +1,5 @@
 // Lexi Anderson
-// Last modified: Nov 10, 2021
+// Last modified: Nov 15, 2021
 // CS 4760, Project 5
 // shm.h -- Implementation of shared memory
 
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 
 #define MAX_USER_PROCS 18
+#define NUM_RSS 20  // number of different resource types
 
 // termination criteria
 #define MAX_PROCS_GENERATED 40
@@ -18,11 +19,20 @@
 // TODO: shared memory, semaphores, message queues
 
 // allocate shared memory for system data structs
-	// resource descriptors
 
 typedef struct {
+	int instances[NUM_RSS];
+	int shareable[NUM_RSS];
 
 } Resource;
+
+typedef struct {
+	pid_t pid;
+	int pidsim;  // simulated pid
+	int allocation[NUM_RSS];  // allocated resources
+	int request[NUM_RSS];  // requested resources
+	int maximum[NUM_RSS];  // max number of instances of resource
+} PCB;
 
 
 #endif
