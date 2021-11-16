bdba85fd8aebfb5b8665dd4a6f914fb635d7a95f
anderav1 <avabc5@mail.umsl.edu>
2021-11-16 11:43:28 -0600

    Working on resource descriptors

--- oss.c
+++ oss.c
@@ -1,7 +1,12 @@
 // Author: Lexi Anderson
-// Last modified: Nov 8, 2021
+// Last modified: Nov 15, 2021
 // CS 4760, Project 5
 // oss.c
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "shared.h"
 
 
 int main(int argc, char** argv) {
